e7a497034c25b09fd49ced25ef5e594891b2bf11
Dennys Hernández Muñiz <dennys.hernandez@enti.cat>
2023-02-24 14:29:38 +0100

    Clase Viernes Pt 2 - OK

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,22 +1,45 @@
+import React from 'react';
+
 import Title from './Title';
 import TaskForm from './TaskForm';
 import TaskList from './TaskList';
 
 import './App.css';
 
-function App() {
-  return (
-	<main className="App">
+class App extends React.Component {
+
+	constructor (props) {
+		super(props);
+
+		this.state = {
+			tasklist: []
+		};
+	}
+
+	addTask = (task) => {
+		console.log(task);	
+		
+		this.state.tasklist.unshift(task);
+		
+		this.setState({
+			tasklist: this.state.tasklist
+		});
+	}
+
+	render(){
+		return (
+			<main className="App">
       	
-		<Title text="ToDo List App"/>
+				<Title text="ToDo List App"/>
 
-		<TaskForm/>
-		<TaskList/>
+				<TaskForm onAddTask={this.addTask}/>
+				<TaskList list={this.state.tasklist}/>
 
-		<p>Tienes <strong>3</strong> tareas pendientes</p>
-      
-	</main>
-  );
+				<p>Tienes <strong>{this.state.tasklist.length}</strong> tareas pendientes</p>
+      	
+			</main>
+		);
+	}
 }
 
 export default App;
